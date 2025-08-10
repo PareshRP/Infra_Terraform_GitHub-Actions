0ffac5e8185fe828a4e4bab8c54916e28098b45b
Paresh Patil <55230753+PareshRP@users.noreply.github.com>
2025-08-11 04:30:54 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,18 @@ provider "aws" {
     region = "us-east-1"
 }
 
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda_code"
+  output_path = "${path.module}/lambda.zip"
+}
+
 resource "aws_s3_bucket" "example" {
-    bucket = "example-bucket"
+    bucket = "example-bucket-${random_id.suffix.hex}"
 }
 
 resource "aws_iam_role" "lambda_exec_role" {
@@ -21,12 +31,12 @@ resource "aws_iam_role" "lambda_exec_role" {
 }
 
 resource "aws_lambda_function" "example_lambda" {
-    function_name = "example-lambda"
-    runtime = "nodejs14.x"
+    function_name = "example-lambda-${random_id.suffix.hex}"
+    runtime = "nodejs18.x"
     handler = "index.handler"
     role = aws_iam_role.lambda_exec.arn
-    filename = "${path.module}/lambda.zip"
-    source_code_hash = filebase64sha256("${path.module}/lambda.zip")
+    filename = data.archive_file.lambda_zip.output_path
+    source_code_hash = data.archive_file.lambda_zip.output_base64sha256
     environment {
         variables = {
             LOG_LEVEL = "info" 
